b97734870f516c5482aea4d4df85dfe9651bfb26
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-04-23 18:06:09 +0700

    Refactor slider logic and fix scroll delta inversion.
    Revised `ScrollDelta` to invert values for consistency. Improved slider thumb positioning and added clamping for calculated values to ensure they stay within the defined range. Simplified `CalculateValue` implementation for readability.

--- src/OG.Element/Interactive/OgSlider.cs
+++ src/OG.Element/Interactive/OgSlider.cs
@@ -9,18 +9,15 @@ namespace OG.Element.Interactive;
 public abstract class OgSlider<TElement, TScope>(string name, TScope scope, IOgTransform transform, float value, IDkRange<float> range, float scrollStep)
     : OgDraggableValueView<TElement, TScope, float>(name, scope, transform, value) where TElement : IOgElement where TScope : IOgTransformScope
 {
-    protected override float CalculateValue(OgEvent reason, float value)
-    {
-        float min = range.Min;
-        float max = range.Max;
-        return Mathf.Lerp(min, max, InverseLerp(Transform.LocalRect, reason.MousePosition));
-    }
+    protected override float CalculateValue(OgEvent reason, float value) => 
+        Mathf.Lerp(range.Min, range.Max, InverseLerp(Transform.LocalRect, reason.MousePosition));
 
     protected override void HandleMouseScroll(OgEvent reason)
     {
         base.HandleMouseScroll(reason);
         if(!IsHovered) return;
-        ChangeValue(Value + scrollStep, reason);
+        var value = Mathf.Clamp(Value + (Mathf.Sign(reason.ScrollDelta.y) * scrollStep), range.Min, range.Max);
+        ChangeValue(value, reason);
     }
 
     protected abstract float InverseLerp(Rect rect, Vector2 mousePosition);
