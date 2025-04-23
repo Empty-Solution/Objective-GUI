369508518161cddfcfbe4295030d99c5f8bdf768
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-04-23 20:08:28 +0700

    idk

--- src/OG.Animation/OgKeyframeCurve.cs
+++ src/OG.Animation/OgKeyframeCurve.cs
@@ -6,16 +6,20 @@ public class OgKeyframeCurve(float min, float max) : OgCurve(min, max)
 {
     public override IOgCurveVertex GetNearestVertex(float time)
     {
-        if(time <= this[0].Time)
-            return this[0];
+        var first = this[0];
+        if(time <= first.Time)
+            return first;
+        var last = this[Count - 1];
+        if(time >= last.Time)
+            return last;
 
-        if(time >= this[Count - 1].Time)
-            return this[Count - 1];
+        for(int i = 1; i < Count - 1; i++)
+        {
+            var next = this[i + 1];
+            if(time >= this[i].Time && time < next.Time)
+                return next;
+        }
 
-        for(int i = 0; i < Count - 1; i++)
-            if(time >= this[i].Time && time < this[i + 1].Time)
-                return this[i + 1];
-
-        return this[Count - 1];
+        return last;
     }
 }
