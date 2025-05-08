0a3d39cf9d847f9534d6989e88d2ff8bcffd19f3
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-08 12:00:05 +0700

    Add OgScriptableTransformer and OgScriptableOption classes
    Introduce `OgScriptableTransformer` for handling transformations with customizable logic, and `OgScriptableOption` for managing associated properties. These additions form the foundation for extensible and configurable transformation behavior.

---
+++ src/OG.Transformer/Options/OgScriptableOption.cs
@@ -0,0 +1,19 @@
+﻿using OG.Transformer.Abstraction;
+using OG.Transformer.Transformers;
+using System.Collections.Generic;
+namespace OG.Transformer.Options;
+public class OgScriptableOption(string name) : IOgTransformerOption
+{
+    private readonly Dictionary<string, object> m_Properties = [];
+    
+    public object? GetProperty(string propertyName) => 
+        m_Properties.TryGetValue(propertyName, out object? property) ? property : null;
+    public T? GetProperty<T>(string propertyName) => (T?)GetProperty(propertyName);
+    public void SetProperty(string propertyName, object value)
+    {
+        if(m_Properties.ContainsKey(propertyName))
+            m_Properties[propertyName] = value;
+        m_Properties.Add(propertyName, value);
+    }
+    public bool CanHandle(IOgTransformer value) => value is OgScriptableTransformer transformer && transformer.Name == name;
+}
