10a8b32e7b3ed3d5f18a1fe9a735cc8fd2d10a9f
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-08 11:10:41 +0700

    nothing

--- src/OG.Transformer/Transformers/OgMaxSizeTransformer.cs
+++ src/OG.Transformer/Transformers/OgMaxSizeTransformer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace OG.Transformer.Transformers;
 public class OgMaxSizeTransformer : OgTransformerBase<OgMaxSizeOption>
 {
-    public override int Order => int.MaxValue;
+    public override int Order => 999;
     public override Rect Transform(Rect rect, Rect parentRect, Rect lastRect, OgMaxSizeOption option) =>
         new(rect.x, rect.y, Mathf.Min(rect.width, option.MaxWidth), Mathf.Min(rect.height, option.MaxHeight));
 }
--- src/OG.Transformer/Transformers/OgMinSizeTransformer.cs
+++ src/OG.Transformer/Transformers/OgMinSizeTransformer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace OG.Transformer.Transformers;
 public class OgMinSizeTransformer : OgTransformerBase<OgMinSizeOption>
 {
-    public override int Order => int.MaxValue;
+    public override int Order => 999;
     public override Rect Transform(Rect rect, Rect parentRect, Rect lastRect, OgMinSizeOption option) =>
         new(rect.x, rect.y, Mathf.Max(rect.width, option.MinWidth), Mathf.Max(rect.height, option.MinHeight));
 }
