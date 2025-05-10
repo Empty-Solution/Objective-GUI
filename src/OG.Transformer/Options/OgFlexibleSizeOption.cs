31f2290b6aa0b9df20c3370154c54bbec8e3f4d6
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-10 17:58:36 +0700

    New transformer

---
+++ src/OG.Transformer/Options/OgFlexibleSizeOption.cs
@@ -0,0 +1,9 @@
+﻿using OG.DataTypes.Orientation;
+using OG.Transformer.Abstraction;
+using OG.Transformer.Transformers;
+namespace OG.Transformer.Options;
+public sealed class OgFlexibleSizeOption : IOgTransformerOption
+{
+    public EOgOrientation Orientation { get; set; } = EOgOrientation.HORIZONTAL;
+    public bool CanHandle(IOgTransformer value) => value is OgFlexibleSizeTransformer;
+}
