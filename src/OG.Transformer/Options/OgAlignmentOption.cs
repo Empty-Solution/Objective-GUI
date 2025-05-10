c76cd6c5be0857b4227c1be2abfa4d000797260c
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-10 08:15:43 +0700

    Add OgAlignmentTransformer and alignment option support
    Introduced `OgAlignmentTransformer` and `OgAlignmentOption` to handle alignment transformations using `TextAnchor`. Updated the project file to include the `UnityEngine.TextRenderingModule` package for alignment-related features. This enhances flexibility and support for alignment adjustments in the transformer system.

---
+++ src/OG.Transformer/Options/OgAlignmentOption.cs
@@ -0,0 +1,9 @@
+﻿using OG.Transformer.Abstraction;
+using OG.Transformer.Transformers;
+using UnityEngine;
+namespace OG.Transformer.Options;
+public class OgAlignmentOption(TextAnchor alignment) : IOgTransformerOption
+{
+    public TextAnchor Alignment { get; set; } = alignment;
+    public bool CanHandle(IOgTransformer value) => value is OgAlignmentTransformer;
+}
