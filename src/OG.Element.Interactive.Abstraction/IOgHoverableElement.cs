a3e028bb4b669f931b42e326545790110f4c26d4
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-13 06:17:34 +0700

    idk

--- src/OG.Element.Interactive.Abstraction/IOgHoverableElement.cs
+++ src/OG.Element.Interactive.Abstraction/IOgHoverableElement.cs
@@ -2,7 +2,7 @@
 using OG.Element.Abstraction;
 using OG.Element.Container.Abstraction;
 namespace OG.Element.Interactive.Abstraction;
-public interface IOgHoverableElement<TElement> : IOgContainer<TElement>, IOgInteractable where TElement : IOgElement
+public interface IOgHoverableElement<TElement> : IOgContainer<TElement> where TElement : IOgElement
 {
     IDkObservableProperty<bool>? IsHovering { get; }
 }
