4db8a210040b3d50bd12a1bc1beddcd6666ff9d1
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-20 10:32:28 +0700

    Add scriptable and refined animation observers - Introduced `OgAnimationScriptableObserver` to enable scriptable updates using custom transformations. - Renamed `OgAnimationTargetObserver` to `OgAnimationObserver` for streamlined naming consistency.

---
+++ src/OG.DataKit.Animation/OgAnimationScriptableObserver.cs
@@ -0,0 +1,12 @@
+﻿using DK.Observing.Abstraction.Generic;
+using System;
+namespace OG.DataKit.Animation;
+public class OgAnimationScriptableObserver<TValue, TObserverValue>(OgAnimationGetter<TValue> getter, Func<TObserverValue, TValue> func)
+    : IDkObserver<TObserverValue> where TValue : notnull
+{
+    public void Update(TObserverValue state) => getter.Target = func.Invoke(state);
+    public void Update(object state)
+    {
+        if(state is TObserverValue casted) Update(casted);
+    }
+}
