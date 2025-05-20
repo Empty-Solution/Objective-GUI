441c8b1cec63fbeabaff40bc23149fe80690f7a7
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-20 14:20:29 +0700

    Removed shitcode

--- src/OG.EventSystem.Abstraction/IOgEventSystem.cs
+++
@@ -1,8 +0,0 @@
-﻿using OG.Event.Abstraction;
-namespace OG.EventSystem.Abstraction;
-public interface IOgEventSystem<TEvent> where TEvent : class
-{
-    void Register(IOgEventPipe<TEvent> pipe);
-    void Unregister(IOgEventPipe<TEvent> pipe);
-    void Handle();
-}
