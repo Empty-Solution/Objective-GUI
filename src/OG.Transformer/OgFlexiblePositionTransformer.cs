d4bf0fd6a4c3408472f32b0fcc836e2b2b43dfcf
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-05-27 04:34:36 +0700

    order

--- src/OG.Transformer/OgFlexiblePositionTransformer.cs
+++ src/OG.Transformer/OgFlexiblePositionTransformer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace OG.Transformer;
 public class OgFlexiblePositionTransformer : OgBaseTransformer<OgFlexiblePositionTransformerOption>
 {
-    public override int Order { get; set; } = 90;
+    public override int Order { get; set; } = 1;
     public override Rect Transform(Rect rect, Rect parentRect, Rect lastRect, int remaining, OgFlexiblePositionTransformerOption option) =>
         option.Orientation == EOgOrientation.HORIZONTAL ? new(lastRect.xMax + option.Padding, rect.y, rect.width, rect.height)
             : new(rect.x, lastRect.yMax + option.Padding, rect.width, rect.height);
