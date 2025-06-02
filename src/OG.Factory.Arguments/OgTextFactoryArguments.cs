5a2a92e39c42c5411b56b945f09df025b98816bd
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-06-03 06:29:07 +0700

    ...

--- src/OG.Factory.Arguments/OgTextFactoryArguments.cs
+++ src/OG.Factory.Arguments/OgTextFactoryArguments.cs
@@ -3,11 +3,11 @@ using OG.Event.Abstraction;
 using UnityEngine;
 namespace OG.Factory.Arguments;
 public class OgTextFactoryArguments(string name, IDkGetProvider<Rect> rectGetProvider, IOgEventHandlerProvider? eventProvider,
-    IDkGetProvider<Color> colorGetter, IDkGetProvider<Font> font, IDkGetProvider<int> fontSize, FontStyle fontStyle, IDkGetProvider<TextAnchor> alignment,
+    IDkGetProvider<Color> colorGetter, IDkGetProvider<Font?> font, IDkGetProvider<int> fontSize, FontStyle fontStyle, IDkGetProvider<TextAnchor> alignment,
     TextClipping textClipping, bool wordWrap, IDkGetProvider<string> text) : OgVisualFactoryArguments(name, rectGetProvider, eventProvider, colorGetter)
 {
     public IDkGetProvider<string>     Text         => text;
-    public IDkGetProvider<Font>       Font         => font;
+    public IDkGetProvider<Font?>       Font         => font;
     public IDkGetProvider<int>        FontSize     => fontSize;
     public FontStyle                  FontStyle    => fontStyle;
     public IDkGetProvider<TextAnchor> Alignment    => alignment;
