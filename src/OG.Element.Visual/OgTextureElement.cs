242c3d7da2d4fee4d62ba3c850ea6fc0c69dfe9f
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-06-03 14:01:08 +0700

    texture fully went to getters

--- src/OG.Element.Visual/OgTextureElement.cs
+++ src/OG.Element.Visual/OgTextureElement.cs
@@ -6,19 +6,19 @@ namespace OG.Element.Visual;
 public class OgTextureElement(string name, IOgEventHandlerProvider provider, IDkGetProvider<Rect> rectGetter)
     : OgVisualElement<OgTextureGraphicsContext>(name, provider, rectGetter)
 {
-    public IDkGetProvider<Color>? ColorProvider  { get; set; }
-    public IDkGetProvider<Texture2D>?      Texture        { get; set; }
-    public Vector4                BorderWidths   { get; set; }
-    public Vector4                BorderRadiuses { get; set; }
-    public float                  ImageAspect    { get; set; }
-    public bool                   AlphaBlend     { get; set; }
+    public IDkGetProvider<Color>?     ColorProvider  { get; set; }
+    public IDkGetProvider<Texture2D>? Texture        { get; set; }
+    public IDkGetProvider<Vector4>?   BorderWidths   { get; set; }
+    public IDkGetProvider<Vector4>?   BorderRadiuses { get; set; }
+    public float                      ImageAspect    { get; set; }
+    public bool                       AlphaBlend     { get; set; }
     protected override void FillContext()
     {
         m_RenderContext                ??= new();
         m_RenderContext.Color          =   ColorProvider!.Get();
         m_RenderContext.Texture        =   Texture?.Get();
-        m_RenderContext.BorderRadiuses =   BorderRadiuses;
-        m_RenderContext.BorderWidths   =   BorderWidths;
+        m_RenderContext.BorderRadiuses =   BorderRadiuses?.Get() ?? new();
+        m_RenderContext.BorderWidths   =   BorderWidths?.Get() ?? new();
         m_RenderContext.ImageAspect    =   ImageAspect;
         m_RenderContext.AlphaBlend     =   AlphaBlend;
     }
--- src/OG.Factory.Arguments/OgTextureFactoryArguments.cs
+++ src/OG.Factory.Arguments/OgTextureFactoryArguments.cs
@@ -3,11 +3,11 @@ using OG.Event.Abstraction;
 using UnityEngine;
 namespace OG.Factory.Arguments;
 public class OgTextureFactoryArguments(string name, IDkGetProvider<Rect> rectGetProvider, IOgEventHandlerProvider? eventProvider,
-    IDkGetProvider<Color> colorGetter, IDkGetProvider<Texture2D> texture, Vector4 borderWidths, Vector4 borderRadiuses, float imageAspect, bool alphaBlend)
+    IDkGetProvider<Color> colorGetter, IDkGetProvider<Texture2D> texture, IDkGetProvider<Vector4>? borderWidths, IDkGetProvider<Vector4>? borderRadiuses, float imageAspect, bool alphaBlend)
     : OgVisualFactoryArguments(name, rectGetProvider, eventProvider, colorGetter)
 {
-    public Vector4                   BorderWidths   { get; } = borderWidths;
-    public Vector4                   BorderRadiuses { get; } = borderRadiuses;
+    public IDkGetProvider<Vector4>?                  BorderWidths   { get; } = borderWidths;
+    public IDkGetProvider<Vector4>? BorderRadiuses { get; } = borderRadiuses;
     public float                     ImageAspect    { get; } = imageAspect;
     public bool                      AlphaBlend     { get; } = alphaBlend;
     public IDkGetProvider<Texture2D> Texture        => texture;
