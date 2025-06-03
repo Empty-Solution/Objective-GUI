c8f5ee381eb67b3c75a3cdf94750c0704166cac0
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-06-03 13:33:49 +0700

    orders

--- src/OG.Transformer/OgRelativeMarginTransformer.cs
+++ src/OG.Transformer/OgRelativeMarginTransformer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace OG.Transformer;
 public class OgRelativeMarginTransformer : OgBaseTransformer<OgRelativeMarginTransformerOption>
 {
-    public override int Order { get; set; } = 20;
+    public override int Order { get; set; } = 19;
     public override Rect Transform(Rect rect, Rect parentRect, Rect lastRect, int remaining, OgRelativeMarginTransformerOption option) =>
         new(option.RelativeMarginX == 0 ? rect.x : parentRect.xMax * option.RelativeMarginX,
             option.RelativeMarginY == 0 ? rect.y : parentRect.yMax * option.RelativeMarginY, rect.width, rect.height);
