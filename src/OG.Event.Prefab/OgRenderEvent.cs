efc933735b67185d9f76adc32eeefac1eda08ed1
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-06-11 23:06:04 +0700

    clipcontext to private

--- src/OG.Event.Prefab/OgRenderEvent.cs
+++ src/OG.Event.Prefab/OgRenderEvent.cs
@@ -64,4 +64,12 @@ public class OgRenderEvent(IEnumerable<IOgGraphics> graphics) : OgEvent, IOgRend
         }
         m_ClipContextIndex = -1;
     }
+    private struct OgClipContext(Rect clipRect, Vector2 global, Vector2 scrollOffset)
+    {
+        public Rect    OriginalClipRect => clipRect;
+        public Vector2 Global           { get; set; } = global;
+        public Vector2 ScrollOffset     => scrollOffset;
+        public Rect GetRectToClip() => new(clipRect.position + Global, clipRect.size);
+        public List<IOgGraphicsContext> Contexts { get; } = [];
+    }
 }
