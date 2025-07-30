3eda55c34d6658bdd66c232b5dea2f8c1180c4b2
Ukine9999 <111376450+Ukine9999@users.noreply.github.com>
2025-07-30 19:37:02 +0700

    scroll fix

--- src/OG.Element.Interactive/OgScroll.cs
+++ src/OG.Element.Interactive/OgScroll.cs
@@ -77,7 +77,7 @@ public class OgScroll<TElement> : OgInteractableValueElement<TElement, Vector2>,
     private bool ProcessElement(IOgEvent reason, Rect rect, TElement element)
     {
         Rect  elementRect = element.ElementRect.Get();
-        float offset      = elementRect.height * 0.7f;
+        float offset      = elementRect.height * 1.2f;
         if(elementRect.yMin + offset < rect.yMin || elementRect.yMax - offset > rect.yMax) return false;
         return element.ProcessEvent(reason);
     }
